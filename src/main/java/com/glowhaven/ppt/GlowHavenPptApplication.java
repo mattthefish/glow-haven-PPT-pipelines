87cb51e1763a792de68b66327abf80a67c5a875c
Matt <131168739+mattthefish@users.noreply.github.com>
2026-04-28 15:24:35 +0100

    Update GlowHavenPptApplication.java

--- src/main/java/com/glowhaven/ppt/GlowHavenPptApplication.java
+++ src/main/java/com/glowhaven/ppt/GlowHavenPptApplication.java
@@ -5,7 +5,7 @@ import static spark.Spark.port;
 
 public class GlowHavenPptApplication {
 
-    public static void main(String[ args) {
+    public static void main(String[] args) {
         port(8080);
         get("/", (req, res) -> "Glow Haven PPT API");
     }
